5de86eae9c6c9e27bf89a9ead4158d767a2ebbc4
Nguyen Anh Toi <toidxbp02@gmail.com>
2025-01-06 11:32:43 +0700

    Controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Admin;
+use App\Services\Interfaces\AdminServiceInterface as AdminService;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
-    //
+    protected $adminService;
+
+    public function __construct(AdminService $adminService)
+    {
+        $this->adminService = $adminService;
+    }
+    public function store(Request $request)
+    {
+        try {
+            $now = date('d-m-Y H:i:s');
+            $stringTime = strtotime($now);
+            DB::table('adminlogs')->insert([
+                'admin_id' => Auth::guard('admin')->user()->id,
+                'time' =>  $stringTime,
+                'ip' => $request->ip() ?? null,
+                'action' => 'add a admin',
+                'cat' => 'admin',
+            ]);
+            $store = $this->adminService->store($request);
+            return $store;
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ], 422);
+        }
+    }
 }
